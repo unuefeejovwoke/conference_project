3fa11b8fb003033f607bc142accdd83dcb9bf0f0
unuefeejovwoke <vuwill7114@gmail.com>
2023-06-19 19:10:00 -0700

    urls for list, create, detail, update and delete

---
+++ conference/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'conference'
+
+urlpatterns = [
+    path('conferences/', views.conference_list, name='conference_list'),
+    path('conferences/create/', views.conference_create, name='conference_create'),
+    path('conferences/<int:conference_id>/', views.conference_detail, name='conference_detail'),
+    path('conferences/<int:conference_id>/update/', views.conference_update, name='conference_update'),
+    path('conferences/<int:conference_id>/delete/', views.conference_delete, name='conference_delete'),
+]
